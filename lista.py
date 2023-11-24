a5a725c0b19c69b73bd0371e2f74e499ffc1979e
Marxouo <78555292+marxeugenio@users.noreply.github.com>
2023-11-23 22:10:41 -0300

    Create lista.py
    Adicionando elementos a uma lista dinamicamente

---
+++ lista.py
@@ -0,0 +1,15 @@
+lista_produtos = []
+
+print(f"Criando uma lista de produtos vazia: {lista_produtos}")
+
+print("Adicionando caneta esferográfica preta á lista:")
+
+lista_produtos.append("caneta esferográfica preta")
+
+print(lista_produtos)
+
+print(f"Adicionando caneta esferográfica azul á lista:")
+
+lista_produtos.append("caneta esferográfica azul")
+
+print(lista_produtos) 
