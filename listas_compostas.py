d636bd323ad21bcdea274b5dcfee6fda83364c3d
Marxouo <78555292+marxeugenio@users.noreply.github.com>
2023-11-23 22:00:23 -0300

    Create listas_compostas.py

---
+++ listas_compostas.py
@@ -0,0 +1,8 @@
+lista_de_compras = ["Arroz", "Banana", "Macarrão", "Cenoura", "Limão"]
+
+lista_de_bebidas = ["Coca","Suco","Sprite","chá"]
+
+
+compras = [lista_de_compras, lista_de_bebidas]
+
+print(f"Conteudo da lista de Compras: {compras}")
