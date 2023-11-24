762d63073ecd47f2e7ee21b84624dab9fb806939
Marxouo <78555292+marxeugenio@users.noreply.github.com>
2023-11-23 22:22:28 -0300

    Create insert.py
    Inserindo elementos em uma lista

---
+++ insert.py
@@ -0,0 +1,16 @@
+# Primeiro, crio uma lista com algumas ferramentas
+
+ferramentas = ["Alicate", "Martelo", "Prego", "Furadeira"]
+
+print(f"Lista de ferramentas: {ferramentas}")
+
+# Agora adiciono mais duas, de forma a mantar a ordem alfabetica
+
+ferramentas.insert(2,"Chave Torx")
+
+print("Comprei uma chave torx: ",ferramentas)
+
+ferramentas.insert(0,"Alicate de corte")
+
+print(f"Comprei um alicate de corte: {ferramentas}")
+
