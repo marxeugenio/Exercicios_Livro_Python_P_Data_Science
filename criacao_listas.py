253f676266fb039fd7c15989ecd11d3c6c9a8cd8
Marxouo <78555292+marxeugenio@users.noreply.github.com>
2023-11-23 21:57:10 -0300

    Create criacao_listas.py

---
+++ criacao_listas.py
@@ -0,0 +1,6 @@
+lista_animes = ["Naruto","Dragon Ball Z","Pokemon", "Demon Slayer","DeathNote"]
+
+print(f"Conteudo da lista de animes: {lista_animes}")
+
+print(f"O primeiro parametro da lista é o: {lista_animes[0]}")
+print(f"O ultimo parametro da lista é o: {lista_animes[-1]}")
