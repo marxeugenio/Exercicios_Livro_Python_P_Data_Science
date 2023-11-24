a044c8c5195002b5899db473d7477bc7f99941fb
Marxouo <78555292+marxeugenio@users.noreply.github.com>
2023-11-23 22:05:48 -0300

    Create arrays_bidimensionais.py
    Acessando listas compostas como Arrays bidimensionais

---
+++ arrays_bidimensionais.py
@@ -0,0 +1,10 @@
+carros_populares = ["Gol","Nissan","Ferrari","Mustang","HB20"]
+superesportivos = ["Fiesta","Citroen","Motoca","bis","Kavazaki"]
+
+todos_os_veiculos = [carros_populares,superesportivos]
+
+print(f"Carros populares: {todos_os_veiculos[0]}")
+
+print(f"Superesportivos: {todos_os_veiculos[1]}")
+
+print(f"Segundo carro popular {todos_os_veiculos[0][2]}")
