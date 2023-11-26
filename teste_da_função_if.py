65e2eb23829d258fe4dc59bf25bf40f2a7fa2e3f
Marxouo <78555292+marxeugenio@users.noreply.github.com>
2023-11-26 16:03:13 -0300

    Create teste_da_função_if.py

---
+++ teste_da_função_if.py
@@ -0,0 +1,12 @@
+x = 5
+if x > 0:
+    print(f"{x} é um número positivo.")
+
+print("Esta linha será impressa todas as vezes.")
+
+x = -1
+
+if x > 0:
+    print(f"{x} é um número positivo.")
+
+print("Esta linha também será impressa sempre") 
