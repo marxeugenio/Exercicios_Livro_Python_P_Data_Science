2e31ad1de149aede23064bd78eb41aa6de0f531c
Marxouo <78555292+marxeugenio@users.noreply.github.com>
2023-11-26 18:15:15 -0300

    Create qual_o_maior.py

---
+++ qual_o_maior.py
@@ -0,0 +1,10 @@
+var1 = 10
+var2 = 20
+var3 = 30
+
+if (var1 > var2) and (var1 >var3):
+    print("O primeiro valor é o maior")
+elif (var2 > var1) and (var2 > var3):
+    print("O segundo valor é o maior")
+else:
+    print("O terceiro valor é o maior")
