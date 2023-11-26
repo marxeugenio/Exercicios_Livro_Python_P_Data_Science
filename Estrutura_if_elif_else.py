de5fc307aa8b717a30058cda10dd7ee996e249f1
Marxouo <78555292+marxeugenio@users.noreply.github.com>
2023-11-26 16:09:24 -0300

    Create Estrutura_if_elif_else.py

---
+++ Estrutura_if_elif_else.py
@@ -0,0 +1,10 @@
+media = float(input("Informe a sua média: "))
+
+if media >= 7.0:
+    print("Parabéns! Você foi aprovado por média")
+
+elif (media < 7.0) and (media >= 3.0):
+    print("Você fara prova final")
+
+else:
+    print("Você foi reprovado")
