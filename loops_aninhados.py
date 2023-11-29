837568404ee445e0a30ac02cbf4842c17b7f511e
Marxouo <78555292+marxeugenio@users.noreply.github.com>
2023-11-28 22:26:28 -0300

    Create loops_aninhados.py
    Usando loops anihados

---
+++ loops_aninhados.py
@@ -0,0 +1,8 @@
+camisetas = ["azul","vermelha","amarela"]
+shorts = ["branco","preto"]
+x = 0
+y = 0
+
+for x in range(3):
+    for y in range(2):
+        print(f"Você pode combinar uma camiseta {camisetas[x]} com um short {shorts[y]}")
