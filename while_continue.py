2ea9a382b2920019f401c9035c5724b3327cd197
Marxouo <78555292+marxeugenio@users.noreply.github.com>
2023-11-28 23:20:31 -0300

    Create while_continue.py

---
+++ while_continue.py
@@ -0,0 +1,9 @@
+while(True):
+    letra = input("Digite alguma letra diferente de X (Q para sair!)")
+    if(letra == "X"):
+        continue
+    elif(letra == "Q"):
+        break
+    else:
+        print(f"Você digitou a {letra}")
+print("Programa encerrado")
