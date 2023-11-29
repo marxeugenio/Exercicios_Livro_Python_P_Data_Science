a2a704a8077f00a9cb4536270aafeb05a92d8d15
Marxouo <78555292+marxeugenio@users.noreply.github.com>
2023-11-28 23:02:24 -0300

    Create loops_alinhados2.py

---
+++ loops_alinhados2.py
@@ -0,0 +1,9 @@
+camisetas = ["Azul","Vermelha","Amarela"]
+shorts = ["branco","preto"]
+
+x = 0
+y = 0
+
+for cor_camiseta in camisetas:
+    for cor_short in shorts:
+        print(f"Voce pode combinar uma camiseta {cor_camiseta} com um short {cor_short}")
